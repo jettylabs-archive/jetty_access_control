8958d6874a4538c078921f6d9e58f38c47ba5396
Isaac Hales <isaac.hales@gmail.com>
2022-08-24 11:51:49 -0600

    Add helper types to facilitate populating the Graph

--- rusty_jetty/jetty_core/src/connectors.rs
+++ rusty_jetty/jetty_core/src/connectors.rs
@@ -1,5 +1,5 @@
 //! Connectors module.
-//! 
+//!
 use anyhow::Result;
 use async_trait::async_trait;
 
@@ -10,7 +10,37 @@ use crate::jetty::{ConnectorConfig, CredentialsBlob};
 pub trait Connector {
     /// Instantiate a Connector from configuration.
     fn new(config: &ConnectorConfig, credentials: &CredentialsBlob) -> Result<Box<Self>>;
-    /// Check if the Connector is properly set up and return the connection 
+    /// Check if the Connector is properly set up and return the connection
     /// status (true for connected, false for not).
     async fn check(&self) -> bool;
+}
+
+/// Enum of identifiers used to resolve user identities
+pub enum UserIdentifier {
+    /// User's first name
+    FirstName,
+    /// User's last name
+    LastName,
+    /// User's full name
+    FullName,
+    /// User's email address
+    Email,
+    /// A platform specific identifier
+    PlatformID,
+}
+
+/// Enum of known asset types
+pub enum AssetType {
+    /// Database Table
+    DBTable,
+    /// Database View
+    DBView,
+    /// Database Schema
+    DBSchema,
+    /// Database
+    DBDB,
+    /// Database Warehouse
+    DBWarehouse,
+    /// A catch-all that can be used by connector implementors
+    Other,
 }
