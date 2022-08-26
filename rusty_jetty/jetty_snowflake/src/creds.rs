e3ecc3faf208c20e9e8f6a530b863b0626f5e081
Jk Jensen <azjkjensen@gmail.com>
2022-08-26 13:11:03 -0700

    make working with snowflake simpler
    Refactor rest stuff out to make it easier to work with snowflake

---
+++ rusty_jetty/jetty_snowflake/src/creds.rs
@@ -0,0 +1,16 @@
+use serde::Deserialize;
+
+/// Credentials for authenticating to Snowflake.
+///
+/// The user sets these up by following Jetty documentation
+/// and pasting their keys into their connector config.
+#[derive(Deserialize, Debug, Default)]
+pub(crate) struct SnowflakeCredentials {
+    pub(crate) account: String,
+    pub(crate) password: String,
+    pub(crate) role: String,
+    pub(crate) user: String,
+    pub(crate) warehouse: String,
+    pub(crate) private_key: String,
+    pub(crate) public_key_fp: String,
+}
