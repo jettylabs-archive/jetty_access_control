84df0dc17098b89761491bf9a94d1d77bfca74a4
Jk Jensen <azjkjensen@gmail.com>
2022-09-21 16:35:17 -0700

    wip

--- rusty_jetty/jetty_tableau/src/nodes/project.rs
+++ rusty_jetty/jetty_tableau/src/nodes/project.rs
@@ -1,19 +1,46 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use super::{FetchPermissions, Permission};
 use crate::rest::{self, FetchJson};
 
 use anyhow::{Context, Result};
+use jetty_core::{
+    connectors::{nodes, AssetType},
+    cual::Cual,
+};
 use serde::Deserialize;
 
 #[derive(Clone, Default, Debug, Deserialize)]
 pub(crate) struct Project {
+    cual: Cual,
     pub id: String,
     pub name: String,
     pub owner_id: String,
     pub parent_project_id: Option<String>,
     pub controlling_permissions_project_id: Option<String>,
     pub permissions: Vec<Permission>,
+}
+
+impl Project {
+    pub(crate) fn new(
+        cual: Cual,
+        id: String,
+        name: String,
+        owner_id: String,
+        parent_project_id: Option<String>,
+        controlling_permissions_project_id: Option<String>,
+        permissions: Vec<Permission>,
+    ) -> Self {
+        Self {
+            cual,
+            id,
+            name,
+            owner_id,
+            parent_project_id,
+            controlling_permissions_project_id,
+            permissions,
+        }
+    }
 }
 
 fn to_node(tc: &rest::TableauRestClient, val: &serde_json::Value) -> Result<super::Project> {
@@ -32,6 +59,11 @@ fn to_node(tc: &rest::TableauRestClient, val: &serde_json::Value) -> Result<supe
         serde_json::from_value(val.to_owned()).context("parsing asset information")?;
 
     Ok(super::Project {
+        cual: Cual::new(format!(
+            "{}/project/{}",
+            tc.get_cual_prefix(),
+            project_info.id
+        )),
         id: project_info.id,
         name: project_info.name,
         owner_id: project_info.owner.id,
@@ -58,10 +90,34 @@ impl FetchPermissions for Project {
     }
 }
 
+impl From<Project> for nodes::Asset {
+    fn from(val: Project) -> Self {
+        nodes::Asset::new(
+            val.cual,
+            val.name,
+            AssetType::Other,
+            // We will add metadata as it's useful.
+            HashMap::new(),
+            // Governing policies will be assigned in the policy.
+            HashSet::new(),
+            // Workbooks are children of their projects.
+            HashSet::from([]),
+            // Children objects will be handled in their respective nodes.
+            HashSet::new(),
+            // Workbooks are derived from their source data.
+            val.sources,
+            HashSet::new(),
+            // No tags at this point.
+            HashSet::new(),
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use anyhow::{Context, Result};
+    use jetty_core::connectors::nodes;
 
     #[tokio::test]
     async fn test_fetching_projects_works() -> Result<()> {
@@ -88,5 +144,33 @@ mod tests {
             println!("{:#?}", v);
         }
         Ok(())
+    }
+
+    #[test]
+    fn test_asset_from_project_works() {
+        let wb = Project::new(
+            Cual::new("".to_owned()),
+            "id".to_owned(),
+            "name".to_owned(),
+            "owner_id".to_owned(),
+            Some("parent_project_id".to_owned()),
+            Some("cp_project_id".to_owned()),
+            vec![],
+        );
+        nodes::Asset::from(wb);
+    }
+
+    #[test]
+    fn test_project_into_asset_works() {
+        let wb = Project::new(
+            Cual::new("".to_owned()),
+            "id".to_owned(),
+            "name".to_owned(),
+            "owner_id".to_owned(),
+            Some("parent_project_id".to_owned()),
+            Some("cp_project_id".to_owned()),
+            vec![],
+        );
+        let a: nodes::Asset = wb.into();
     }
 }
