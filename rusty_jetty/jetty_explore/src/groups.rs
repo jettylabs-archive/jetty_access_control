4e798aee281a53cd3f93f20a174183456bfd57be
Isaac Hales <isaac.hales@gmail.com>
2022-10-12 12:26:52 -0600

    updated inheritedmembersof backend

--- rusty_jetty/jetty_explore/src/groups.rs
+++ rusty_jetty/jetty_explore/src/groups.rs
@@ -1,7 +1,8 @@
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
 use axum::{extract::Path, routing::get, Extension, Json, Router};
 use jetty_core::access_graph::{self, EdgeType, JettyNode, NodeName};
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
 /// Return a router to handle all group-related requests
@@ -27,6 +28,8 @@ async fn direct_groups_handler(
 ) -> Json<Vec<access_graph::GroupAttributes>> {
     let from = NodeName::Group(node_id);
 
+    println!("{:?}", ag.extract_graph(&from, 1).dot());
+
     let group_nodes = ag.get_matching_children(
         &from,
         |n| matches!(n, EdgeType::MemberOf),
@@ -51,23 +54,43 @@ async fn direct_groups_handler(
 }
 
 /// Return the groups that this group is an inherited member of
-async fn inherited_groups_handler() -> Json<Value> {
-    Json(json! {
-    [
-        {
-          "name": "Frozen Yogurt",
-          "platforms": ["snowflake"],
-          "membership_paths": ["group 1 > group 2 > group name",
-          ]
-        },
-        {
-          "name": "Ice cream sandwich",
-          "platforms": ["Tableau", "jetty"],
-          "membership_paths": ["group 1 > group 2 > group name",
-          "group 3 > group 4 > group name"]
-        },
-      ]
+async fn inherited_groups_handler(
+    Path(node_id): Path<String>,
+    Extension(ag): Extension<Arc<access_graph::AccessGraph>>,
+) -> Json<Value> {
+    #[derive(Serialize, Deserialize)]
+    struct GroupWithPath {
+        name: String,
+        connectors: HashSet<String>,
+        membership_paths: Vec<String>,
+    }
+
+    let from = NodeName::Group(node_id);
+
+    let res = ag.all_matching_simple_paths_to_children(
+        &from,
+        |n| matches!(n, EdgeType::MemberOf),
+        |n| matches!(n, JettyNode::Group(_)),
+        |n| matches!(n, JettyNode::Group(_)),
+        None,
+        None,
+    );
+
+    let group_attributes = res
+        .into_iter()
+        .filter_map(|(n, p)| {
+            if let JettyNode::Group(g) = n {
+                Some(GroupWithPath {
+                    name: g.name.to_owned(),
+                    connectors: g.connectors,
+                    membership_paths: p.iter().map(|p| p.to_string()).collect(),
+                })
+            } else {
+                None
+            }
         })
+        .collect::<Vec<_>>();
+    Json(serde_json::to_value(&group_attributes).unwrap())
 }
 
 /// Return the groups that are direct members of this group
