cd64c7650bc17a1cf9f4f03d5dd22c84cfad3c26
Isaac Hales <isaac.hales@gmail.com>
2022-10-21 08:17:58 -0600

    add get_attributes for typed indicies

--- rusty_jetty/jetty_core/src/access_graph/graph/typed_indices.rs
+++ rusty_jetty/jetty_core/src/access_graph/graph/typed_indices.rs
@@ -1,6 +1,8 @@
 //! Typed graph indices
 //!
-use crate::access_graph::NodeIndex;
+use crate::access_graph::{
+    AccessGraph, AssetAttributes, GroupAttributes, NodeIndex, TagAttributes, UserAttributes,
+};
 
 pub(crate) trait ToNodeIndex {
     fn get_index(&self) -> NodeIndex;
@@ -12,39 +14,81 @@ impl ToNodeIndex for NodeIndex {
     }
 }
 
+/// Implements the ToNodeIndex trait for one or more types that have an `idx` field.
+macro_rules! impl_to_node_index {
+    (for $($t:ty),+) => {
+        $(impl ToNodeIndex for $t {
+            fn get_index(&self) -> NodeIndex {
+                self.idx
+            }
+        })*
+    }
+}
+
+impl_to_node_index!(for AssetIndex, UserIndex, TagIndex, GroupIndex);
+
+/// Index to an Asset node in the AccessGraph
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
-struct AssetIndex {
+pub struct AssetIndex {
     idx: NodeIndex,
 }
-impl ToNodeIndex for AssetIndex {
-    fn get_index(&self) -> NodeIndex {
-        self.idx
+
+impl AssetIndex {
+    /// get reference ot the AssetAttributes corresponding to the node index
+    pub fn get_attributes<'a>(&self, ag: &'a AccessGraph) -> &'a AssetAttributes {
+        let x = &ag[*self];
+        match x {
+            crate::access_graph::JettyNode::Asset(a) => a,
+            _ => panic!("mismatch in node type; expected asset"),
+        }
     }
 }
 
-struct UserIndex {
+/// Index to an User node in the AccessGraph
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UserIndex {
     idx: NodeIndex,
 }
-impl ToNodeIndex for UserIndex {
-    fn get_index(&self) -> NodeIndex {
-        self.idx
+
+impl UserIndex {
+    /// get reference ot the UserAttributes corresponding to the node index
+    pub fn get_attributes<'a>(&self, ag: &'a AccessGraph) -> &'a UserAttributes {
+        let x = &ag[*self];
+        match x {
+            crate::access_graph::JettyNode::User(a) => a,
+            _ => panic!("mismatch in node type; expected user"),
+        }
     }
 }
 
+/// Index to an Group node in the AccessGraph
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 struct GroupIndex {
     idx: NodeIndex,
 }
-impl ToNodeIndex for GroupIndex {
-    fn get_index(&self) -> NodeIndex {
-        self.idx
+impl GroupIndex {
+    /// get reference ot the GroupAttributes corresponding to the node index
+    pub fn get_attributes<'a>(&self, ag: &'a AccessGraph) -> &'a GroupAttributes {
+        let x = &ag[*self];
+        match x {
+            crate::access_graph::JettyNode::Group(a) => a,
+            _ => panic!("mismatch in node type; expected group"),
+        }
     }
 }
 
+/// Index to an Tag node in the AccessGraph
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 struct TagIndex {
     idx: NodeIndex,
 }
-impl ToNodeIndex for TagIndex {
-    fn get_index(&self) -> NodeIndex {
-        self.idx
+impl TagIndex {
+    /// get reference ot the TagAttributes corresponding to the node index
+    pub fn get_attributes<'a>(&self, ag: &'a AccessGraph) -> &'a TagAttributes {
+        let x = &ag[*self];
+        match x {
+            crate::access_graph::JettyNode::Tag(a) => a,
+            _ => panic!("mismatch in node type; expected tag"),
+        }
     }
 }
