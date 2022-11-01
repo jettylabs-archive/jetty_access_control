0d80350d8daf1b670d92636a62f6e0ab2b1cf496
Isaac Hales <isaac.hales@gmail.com>
2022-10-31 21:55:06 -0600

    run fetches based on jetty_config connectors

--- rusty_jetty/jetty_cli/src/lib/lib.rs
+++ rusty_jetty/jetty_cli/src/lib/lib.rs
@@ -9,6 +9,7 @@ mod project;
 mod tui;
 
 use std::{
+    collections::HashMap,
     path::{Path, PathBuf},
     sync::Arc,
     time::Instant,
@@ -54,8 +55,8 @@ enum JettyCommand {
         #[clap(short, long, value_parser, default_value = "false")]
         visualize: bool,
         /// Connectors to collect for.
-        #[clap(short, long, use_value_delimiter=true, value_delimiter=',', default_values_t = vec!["snowflake".to_owned(),"tableau".to_owned(),"dbt".to_owned()])]
-        connectors: Vec<String>,
+        #[clap(short, long, use_value_delimiter = true, value_delimiter = ',')]
+        connectors: Option<Vec<String>>,
     },
     Explore {
         #[clap(short, long, value_parser, default_value = "false")]
@@ -88,15 +89,7 @@ pub async fn cli() -> Result<()> {
         JettyCommand::Explore { fetch_first } => {
             if *fetch_first {
                 info!("Fetching all data first.");
-                fetch(
-                    &vec![
-                        "snowflake".to_owned(),
-                        "tableau".to_owned(),
-                        "dbt".to_owned(),
-                    ],
-                    &false,
-                )
-                .await?;
+                fetch(&None, &false).await?;
             }
             match AccessGraph::deserialize_graph(
                 project::data_dir().join(project::graph_filename()),
@@ -135,80 +128,89 @@ pub async fn cli() -> Result<()> {
     Ok(())
 }
 
-async fn fetch(connectors: &Vec<String>, &visualize: &bool) -> Result<()> {
+async fn fetch(connectors: &Option<Vec<String>>, &visualize: &bool) -> Result<()> {
     let jetty = Jetty::new(project::jetty_cfg_path_local(), project::data_dir())?;
     let creds = fetch_credentials(project::connector_cfg_path())?;
 
-    if connectors.is_empty() {
-        warn!("No connectors, huh?");
-        bail!("Select a connector");
-    }
-
     let mut data_from_connectors = vec![];
 
-    if connectors.contains(&"dbt".to_owned()) {
-        info!("initializing dbt");
-        let now = Instant::now();
-        // Initialize connectors
-        let mut dbt = jetty_dbt::DbtConnector::new(
-            &jetty.config.connectors[&ConnectorNamespace("dbt".to_owned())],
-            &creds["dbt"],
-            Some(ConnectorClient::Core),
-            project::data_dir().join("dbt"),
-        )
-        .await?;
-        info!("dbt took {} seconds", now.elapsed().as_secs_f32());
+    let selected_connectors;
 
-        info!("getting dbt data");
-        let now = Instant::now();
-        let dbt_data = dbt.get_data().await;
-        let dbt_pcd = (dbt_data, ConnectorNamespace("dbt".to_owned()));
-        info!("dbt data took {} seconds", now.elapsed().as_secs_f32());
-        data_from_connectors.push(dbt_pcd);
+    if let Some(conns) = connectors {
+        selected_connectors = jetty
+            .config
+            .connectors
+            .into_iter()
+            .filter(|(name, _config)| conns.contains(&name.to_string()))
+            .collect::<HashMap<_, _>>();
+    } else {
+        selected_connectors = jetty.config.connectors
     }
+    for (namespace, config) in &selected_connectors {
+        match config.connector_type.as_str() {
+            "dbt" => {
+                let mut dbt = jetty_dbt::DbtConnector::new(
+                    &selected_connectors[namespace],
+                    &creds[namespace.to_string().as_str()],
+                    Some(ConnectorClient::Core),
+                    project::data_dir().join(namespace.to_string()),
+                )
+                .await?;
 
-    if connectors.contains(&"snowflake".to_owned()) {
-        info!("intializing snowflake");
-        let now = Instant::now();
-        let mut snow = jetty_snowflake::SnowflakeConnector::new(
-            &jetty.config.connectors[&ConnectorNamespace("snow".to_owned())],
-            &creds["snow"],
-            Some(ConnectorClient::Core),
-            project::data_dir().join("snowflake"),
-        )
-        .await?;
-        info!("snowflake took {} seconds", now.elapsed().as_secs_f32());
+                info!("getting {} data", namespace);
+                let now = Instant::now();
+                let dbt_data = dbt.get_data().await;
+                let dbt_pcd = (dbt_data, namespace.to_owned());
+                info!(
+                    "{} data took {} seconds",
+                    namespace,
+                    now.elapsed().as_secs_f32()
+                );
+                data_from_connectors.push(dbt_pcd);
+            }
+            "snowflake" => {
+                let mut snow = jetty_snowflake::SnowflakeConnector::new(
+                    &selected_connectors[namespace],
+                    &creds[namespace.to_string().as_str()],
+                    Some(ConnectorClient::Core),
+                    project::data_dir().join(namespace.to_string()),
+                )
+                .await?;
 
-        info!("getting snowflake data");
-        let now = Instant::now();
-        let snow_data = snow.get_data().await;
-        let snow_pcd = (snow_data, ConnectorNamespace("snowflake".to_owned()));
-        info!(
-            "snowflake data took {} seconds",
-            now.elapsed().as_secs_f32()
-        );
-        data_from_connectors.push(snow_pcd);
-    }
+                info!("getting {} data", namespace);
+                let now = Instant::now();
+                let snow_data = snow.get_data().await;
+                let snow_pcd = (snow_data, namespace.to_owned());
+                info!(
+                    "{} data took {} seconds",
+                    namespace,
+                    now.elapsed().as_secs_f32()
+                );
+                data_from_connectors.push(snow_pcd);
+            }
+            "tableau" => {
+                let mut tab = jetty_tableau::TableauConnector::new(
+                    &selected_connectors[namespace],
+                    &creds[namespace.to_string().as_str()],
+                    Some(ConnectorClient::Core),
+                    project::data_dir().join(namespace.to_string()),
+                )
+                .await?;
 
-    if connectors.contains(&"tableau".to_owned()) {
-        info!("initializing tableau");
-        let now = Instant::now();
-        let mut tab = jetty_tableau::TableauConnector::new(
-            &jetty.config.connectors[&ConnectorNamespace("tableau".to_owned())],
-            &creds["tableau"],
-            Some(ConnectorClient::Core),
-            project::data_dir().join("tableau"),
-        )
-        .await?;
-        info!("tableau took {} seconds", now.elapsed().as_secs_f32());
-
-        info!("getting tableau data");
-        let now = Instant::now();
-        tab.setup().await?;
-        let tab_data = tab.get_data().await;
-        let tab_pcd = (tab_data, ConnectorNamespace("tableau".to_owned()));
-        info!("tableau data took {} seconds", now.elapsed().as_secs_f32());
-        data_from_connectors.push(tab_pcd);
+                info!("getting {} data", namespace);
+                let now = Instant::now();
+                tab.setup().await?;
+                let tab_data = tab.get_data().await;
+                let tab_pcd = (tab_data, namespace.to_owned());
+                info!(
+                    "{} data took {} seconds",
+                    namespace,
+                    now.elapsed().as_secs_f32()
+                );
+                data_from_connectors.push(tab_pcd);
+            }
+            o => bail!("unknown connector type: {o}"),
+        }
     }
 
     info!("creating access graph");
