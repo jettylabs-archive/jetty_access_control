c9f96499cef698ad0b9da00f75ce4e705aca643e
Isaac Hales <isaac.hales@gmail.com>
2023-01-04 20:30:09 -0700

    rename to asset type

--- rusty_jetty/jetty_core/src/write/assets.rs
+++ rusty_jetty/jetty_core/src/write/assets.rs
@@ -67,7 +67,7 @@ pub(crate) struct YamlAssetDoc {
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub(crate) struct YamlAssetIdentifier {
     name: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", rename = "asset type")]
     asset_type: Option<AssetType>,
     connector: ConnectorNamespace,
     id: String,
